8a80ccb7b0429d6e730c6258dcd8bb49283cde54
Dmitriy Orteney <domovoy1905@gmail.com>
2021-05-20 22:26:39 +0700

    Update input_dialog.dart
    update for flutter 2.2

--- lib/ui/input_dialog.dart
+++ lib/ui/input_dialog.dart
@@ -44,7 +44,7 @@ class _InputDialogState extends State<InputDialog> {
           top: 16,
           left: 16,
           right: 16,
-          bottom: 16,
+          bottom: 16 + MediaQuery.of(context).viewInsets.bottom,
         ),
         child: TextField(
           controller: textController,
